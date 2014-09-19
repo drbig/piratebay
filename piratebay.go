a99cf49f279f8873634ddd94f12627e2ec1b060a
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-09-19 22:35:36 +0200

    Add Ordering support

--- piratebay.go
+++ piratebay.go
@@ -10,8 +10,9 @@ import (
 
 const (
 	ROOTURI        = "http://thepiratebay.org"
-	CATEGORYURI    = "/recent"
+	INFRAURI       = "/search/a/0/99/0"
 	CATEGORYREGEXP = "<opt.*? (.*?)=\"(.*?)\">([A-Za-z- ()/]+)?<?"
+	ORDERINGREGEXP = "/(\\d+)/0\" title=\"Order by (.*?)\""
 	SEARCHURI      = "/search/%s/0/%s/%s"
 	SEARCHREGEXP   = "<a href=\"/torrent/(\\d+)/.*?\" class=\"detLink\" title=\".*?\">(.*?)</a>.</div>.<a href=\"(.*?)\".*?Uploaded <?b?>?(.*?)<?/?b?>?, Size (.*?), ULed.*?\"right\">(.*?)</td>.*?\"right\">(.*?)</td>"
 	INFOURI        = "/ajax_details_filelist.php?id=%s"
@@ -25,7 +26,16 @@ type Category struct {
 }
 
 func (c *Category) String() string {
-  return fmt.Sprintf("%s/%s", c.Group, c.Title)
+	return fmt.Sprintf("%s/%s", c.Group, c.Title)
+}
+
+type Ordering struct {
+	Title string
+	ID    string
+}
+
+func (o *Ordering) String() string {
+	return fmt.Sprintf("%s", o.Title)
 }
 
 type File struct {
@@ -35,26 +45,32 @@ type File struct {
 
 type Site struct {
 	RootURI        string
-	CategoryURI    string
+	InfraURI       string
 	SearchURI      string
 	InfoURI        string
 	CategoryREGEXP *regexp.Regexp
+	OrderingREGEXP *regexp.Regexp
 	SearchREGEXP   *regexp.Regexp
 	InfoREGEXP     *regexp.Regexp
 	Categories     map[string]map[string]string
+	Orderings      map[string]string
 	Client         *http.Client
+
+	infraData string
 }
 
 func NewSite() *Site {
 	return &Site{
 		RootURI:        ROOTURI,
-		CategoryURI:    CATEGORYURI,
+		InfraURI:       INFRAURI,
 		SearchURI:      SEARCHURI,
 		InfoURI:        INFOURI,
 		CategoryREGEXP: regexp.MustCompile(CATEGORYREGEXP),
+		OrderingREGEXP: regexp.MustCompile(ORDERINGREGEXP),
 		SearchREGEXP:   regexp.MustCompile(SEARCHREGEXP),
 		InfoREGEXP:     regexp.MustCompile(INFOREGEXP),
 		Categories:     nil,
+		Orderings:      nil,
 		Client:         &http.Client{},
 	}
 }
@@ -63,20 +79,41 @@ func (s *Site) String() string {
 	return fmt.Sprintf("%s", s.RootURI)
 }
 
-func (s *Site) UpdateCategories() error {
-	res, err := s.Client.Get(s.RootURI + s.CategoryURI)
+func (s *Site) getInfraData() (string, error) {
+	if s.infraData != "" {
+		return s.infraData, nil
+	}
+	res, err := s.Client.Get(s.RootURI + s.InfraURI)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		return fmt.Errorf("Unsuccessful request: %d", res.StatusCode)
+		return "", fmt.Errorf("Unsuccessful request: %d", res.StatusCode)
 	}
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
+		return "", err
+	}
+	s.infraData = string(data)
+	return s.infraData, nil
+}
+
+func (s *Site) UpdateCategories() error {
+	data, err := s.getInfraData()
+	if err != nil {
 		return err
 	}
-	s.parseCategories(string(data))
+	s.parseCategories(data)
+	return nil
+}
+
+func (s *Site) UpdateOrderings() error {
+	data, err := s.getInfraData()
+	if err != nil {
+		return err
+	}
+	s.parseOrderings(data)
 	return nil
 }
 
@@ -99,9 +136,18 @@ func (s *Site) parseCategories(input string) {
 	return
 }
 
+func (s *Site) parseOrderings(input string) {
+	s.Orderings = make(map[string]string, 9)
+	for _, match := range s.OrderingREGEXP.FindAllStringSubmatch(input, -1) {
+		ordering := strings.ToLower(match[2])
+		s.Orderings[ordering] = match[1]
+	}
+	return
+}
+
 func (s *Site) FindCategory(group string, category string) (*Category, error) {
-  if s.Categories == nil {
-    return nil, fmt.Errorf("Categories not loaded")
+	if s.Categories == nil {
+		return nil, fmt.Errorf("Categories not loaded")
   }
   if category == "" {
     return nil, fmt.Errorf("Category not specified")
@@ -112,31 +158,51 @@ func (s *Site) FindCategory(group string, category string) (*Category, error) {
       return nil, fmt.Errorf("Category group '%s' not found", group)
     }
     value, present := categories[category]
-    if !present {
-      return nil, fmt.Errorf("Category '%s/%s' not found", group, category)
-    }
-    return &Category{
-      Group: group,
-      Title: category,
-      ID: value,
-    }, nil
+		if !present {
+			return nil, fmt.Errorf("Category '%s/%s' not found", group, category)
+		}
+		return &Category{
+			Group: group,
+			Title: category,
+			ID:    value,
+		}, nil
+	}
+	var foundCat *Category
+	for group, categories := range s.Categories {
+		for cat, value := range categories {
+			if cat == category {
+				if foundCat != nil {
+					return nil, fmt.Errorf("Category '%s' is ambiguous, please specify group", category)
+				}
+				foundCat = &Category{
+					Group: group,
+					Title: category,
+					ID:    value,
+				}
+			}
+		}
+	}
+	if foundCat == nil {
+		return nil, fmt.Errorf("Category '%s' not found", category)
+	}
+	return foundCat, nil
+}
+
+func (s *Site) FindOrdering(ordering string) (*Ordering, error) {
+	if s.Orderings == nil {
+		return nil, fmt.Errorf("Orderings not loaded")
+	}
+	if ordering == "" {
+	  return nil, fmt.Errorf("Ordering not specified")
   }
-  var foundCat *Category
-  for group, categories := range s.Categories {
-    for cat, value := range categories {
-      if cat == category {
-        if foundCat != nil {
-          return nil, fmt.Errorf("Category '%s' is ambiguous, please specify group", category)
-        }
-        foundCat = &Category{
-          Group: group,
-          Title: category,
-          ID: value,
-        }
-      }
-    }
-  }
-  return foundCat, nil
+	value, present := s.Orderings[ordering]
+	if !present {
+		return nil, fmt.Errorf("Ordering '%s' not found", ordering)
+	}
+	return &Ordering{
+		Title: ordering,
+		ID:    value,
+	}, nil
 }
 
 type Torrent struct {
--- piratebay_test.go
+++ piratebay_test.go
@@ -38,88 +38,162 @@ func TestCategoriesFake(t *testing.T) {
 					<option value="399">Other OS</option>
 				</optgroup>
 `
-  catsRaw := map[string]map[string]string{
-    "": map[string]string{
-      "all":"0",
-    },
-    "audio": map[string]string{
-		  "music":       "101",
-		  "audio books": "102",
-    },
-    "video": map[string]string{
-		  "hd - movies":   "207",
-		  "hd - tv shows": "208",
-    },
-    "applications": map[string]string{
-		  "ios (ipad/iphone)": "305",
-    },
-  }
+	catsRaw := map[string]map[string]string{
+		"": map[string]string{
+			"all": "0",
+		},
+		"audio": map[string]string{
+			"music":       "101",
+			"audio books": "102",
+		},
+		"video": map[string]string{
+			"hd - movies":   "207",
+			"hd - tv shows": "208",
+		},
+		"applications": map[string]string{
+			"ios (ipad/iphone)": "305",
+		},
+	}
 
-  // raw categories parsing
+	// raw categories parsing
 	s := NewSite()
 	s.parseCategories(input)
-  for group, props := range catsRaw {
-    if _, present := s.Categories[group]; !present {
+	for group, props := range catsRaw {
+		if _, present := s.Categories[group]; !present {
 			t.Errorf("Raw category group '%s' not found", group)
 			continue
-    }
-    for k, v := range props {
-      value, present := s.Categories[group][k]
-      if !present {
-        t.Errorf("Raw category '%s/%s' not found", group, k)
-        continue
-      }
-      if value != v {
-        t.Errorf("Raw category '%s/%s' value mismatch: %s != %s", group, k, value, v)
-      }
-    }
-  }
+		}
+		for k, v := range props {
+			value, present := s.Categories[group][k]
+			if !present {
+				t.Errorf("Raw category '%s/%s' not found", group, k)
+				continue
+			}
+			if value != v {
+				t.Errorf("Raw category '%s/%s' value mismatch: %s != %s", group, k, value, v)
+			}
+		}
+	}
 
-  // category finder - uniques
-  for group, props := range catsRaw {
-    if _, present := s.Categories[group]; !present {
+	// category finder - uniques
+	for group, props := range catsRaw {
+		if _, present := s.Categories[group]; !present {
 			continue
+		}
+		for k, v := range props {
+			cat, err := s.FindCategory("", k)
+			if err != nil {
+				t.Errorf("Didn't find unique category '%s/%s': %s", group, k, err)
+				continue
+			}
+			if cat.ID != v {
+				t.Errorf("Category '%s/%s' value mismatch: %s != %s", group, k, v, cat.ID)
+			}
+		}
+	}
+
+	// category finder - non-uniques
+	cat, err := s.FindCategory("", "whatever")
+	if err == nil {
+		t.Errorf("Found non-unique nonexistent category 'whatever': %s", cat)
+	}
+	cat, err = s.FindCategory("", "other")
+	if err == nil {
+		t.Errorf("Found non-unique category 'other': %s", cat)
+	}
+	cat, err = s.FindCategory("video", "other")
+	if err != nil {
+		t.Errorf("Didn'f find fully-qualified category 'video/other': %s", err)
+	}
+
+	// category finder - broken queries
+	_, err = s.FindCategory("audio", "")
+	if err == nil {
+    t.Errorf("Didn't fail with empty category (1): %s", cat)
+	}
+	_, err = s.FindCategory("", "")
+	if err == nil {
+    t.Errorf("Didn't fail with empty category (2): %s", cat)
+	}
+	_, err = s.FindCategory("whatever", "whatever")
+	if err == nil {
+		t.Errorf("Didn't fail with nonexistent category group")
+	}
+	_, err = s.FindCategory("video", "whatever")
+	if err == nil {
+		t.Errorf("Didn't fail with nonexistent category")
+	}
+
+	// category finder - no categories
+	s = NewSite()
+	_, err = s.FindCategory("", "all")
+	if err == nil {
+		t.Errorf("Didn't fail with no categories loaded")
+	}
+}
+
+func TestOrderingFake(t *testing.T) {
+  input := `
+	<thead id="tableHead">
+		<tr class="header">
+			<th><a href="/search/a/0/13/0" title="Order by Type">Type</a></th>
+			<th><div class="sortby"><a href="/search/a/0/1/0" title="Order by Name">Name</a> (Order by: <a href="/search/a/0/3/0" title="Order by Uploaded">Uploaded</a>, <a href="/search/a/0/5/0" title="Order by Size">Size</a>, <span style="white-space: nowrap;"><a href="/search/a/0/11/0" title="Order by ULed by">ULed by</a></span>, <a href="/search/a/0/7/0" title="Order by Seeders">SE</a>, <a href="/search/a/0/9/0" title="Order by Leechers">LE</a>)</div><div class="viewswitch"> View: <a href="/switchview.php?view=s">Single</a> / Double&nbsp;</div></th>
+			<th><abbr title="Seeders"><a href="/search/a/0/7/0" title="Order by Seeders">SE</a></abbr></th>
+			<th><abbr title="Leechers"><a href="/search/a/0/9/0" title="Order by Leechers">LE</a></abbr></th>
+		</tr>
+	</thead>
+`
+  ordrRaw := map[string]string{
+    "type": "13",
+    "name": "1",
+    "uploaded": "3",
+    "size": "5",
+    "seeders": "7",
+    "leechers": "9",
+    "uled by": "11",
+  }
+
+  s := NewSite()
+  s.parseOrderings(input)
+
+  // raw orderings parsing
+  for title, v := range ordrRaw {
+    value, present := s.Orderings[title]
+    if !present {
+      t.Errorf("Raw ordering '%s' not found", title)
+      continue
     }
-    for k, v := range props {
-      cat, err := s.FindCategory("", k)
-      if err != nil {
-        t.Errorf("Didn't find unique category '%s/%s': %s", group, k, err)
-        continue
-      }
-      if cat.ID != v {
-        t.Errorf("Category '%s/%s' value mismatch: %s != %s", group, k, v, cat.ID)
-      }
+    if value != v {
+      t.Errorf("Raw ordering '%s' value mismatch: %s != %s", title, value, v)
     }
   }
 
-  // category finder - non-uniques
-  cat, err := s.FindCategory("", "other")
-  if err == nil {
-    t.Errorf("Found non-unique category 'other': %s", cat)
-  }
-  cat, err = s.FindCategory("video", "other")
-  if err != nil {
-    t.Errorf("Didn'f find fully-qualified category 'video/other': %s", err)
-  }
-
-  // category finder - broken queries
-  cat, err = s.FindCategory("", "")
-  if err == nil {
-    t.Errorf("Didn't fail with empty categories loaded")
-  }
-  cat, err = s.FindCategory("whatever", "whatever")
-  if err == nil {
-    t.Errorf("Didn't fail with nonexistent category group")
-  }
-  cat, err = s.FindCategory("video", "whatever")
-  if err == nil {
-    t.Errorf("Didn't fail with nonexistent category")
+  // ordering finder
+  for title, v := range ordrRaw {
+    ordr, err := s.FindOrdering(title)
+    if err != nil {
+      t.Errorf("Didn't find ordering '%s'", title)
+      continue
+    }
+    if ordr.ID != v {
+      t.Errorf("Ordering '%s' value mismatch: %s != %s", title, ordr.ID, v)
+    }
   }
 
-  // category finder - no categories
+  // ordering finder - broken queries
+  _, err := s.FindOrdering("")
+  if err == nil {
+    t.Errorf("Didn't fail with empty ordering")
+  }
+  _, err = s.FindOrdering("whatever")
+  if err == nil {
+    t.Errorf("Didn't fail with nonexistent ordering")
+  }
+
+  // ordering finder - no orderings
   s = NewSite()
-  _, err = s.FindCategory("", "all")
+  _, err = s.FindOrdering("whatever")
   if err == nil {
-    t.Errorf("Didn't fail with no categories loaded")
+    t.Errorf("Didn't fail with no orderings loaded")
   }
 }
