0c46a61957e8d5bf5203fab847e967af8a3c6fe3
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-09-21 22:55:15 +0200

    Add files filter

--- piratebay.go
+++ piratebay.go
@@ -13,8 +13,8 @@ import (
 )
 
 const (
-	VERSION = "0.0.1"
-  FILTERSEP = ":"
+	VERSION   = "0.0.1"
+	FILTERSEP = ":"
 )
 
 const (
@@ -48,7 +48,7 @@ type Filter struct {
 }
 
 func (f Filter) String() string {
-  return fmt.Sprintf("%s(%s) - %s", f.Name, f.Args, f.Desc)
+	return fmt.Sprintf("%s(%s) - %s", f.Name, f.Args, f.Desc)
 }
 
 func RegisterFilter(f Filter) {
@@ -66,8 +66,8 @@ func init() {
 func initFilters() {
 	RegisterFilter(Filter{
 		Name: "seeders",
-    Args: "min - int, max - int",
-    Desc: "Filter by torrent min/max seeders",
+		Args: "min - int | max - int",
+		Desc: "Filter by torrent min/max seeders",
 		Init: func(arg, value string) (FilterFunc, error) {
 			valueInt, err := strconv.Atoi(value)
 			if err != nil {
@@ -87,63 +87,102 @@ func initFilters() {
 			}
 		},
 	})
+
+	RegisterFilter(Filter{
+		Name: "files",
+		Args: "include - regexp | exclude - regexp",
+		Desc: "Filter by torrent files' name include/exclude",
+		Init: func(arg, value string) (FilterFunc, error) {
+			regexp, err := regexp.Compile(value)
+			if err != nil {
+				return nil, err
+			}
+			switch arg {
+			case "exclude":
+				return func(tr *Torrent) bool {
+					ok := true
+					for _, f := range tr.Files {
+						if regexp.MatchString(f.Path) {
+							ok = false
+							break
+						}
+					}
+					return ok
+				}, nil
+			case "include":
+				return func(tr *Torrent) bool {
+					tr.GetFiles()
+					ok := false
+					for _, f := range tr.Files {
+						if regexp.MatchString(f.Path) {
+							ok = true
+							break
+						}
+					}
+					return ok
+				}, nil
+			default:
+				return nil, fmt.Errorf("Unknown arg '%s'", arg)
+			}
+		},
+	})
 }
 
 func SetupFilters(fs []string) ([]FilterFunc, error) {
-  var out []FilterFunc
-  for _, f := range fs {
-    parts := strings.Split(f, FILTERSEP)
-    switch len(parts) {
-    case 1:
-      filter, present := filters[parts[0]]
-      if !present {
-        return out, fmt.Errorf("Filter '%s' not found", parts[0])
-      }
-      fFunc, err := filter.Init("", "")
-      if err != nil {
-        return out, fmt.Errorf("Setup failed for filter '%s': %s", parts[0], err)
-      }
-      out = append(out, fFunc)
-    case 3:
-      filter, present := filters[parts[0]]
-      if !present {
-        return out, fmt.Errorf("Filter '%s' not found", parts[0])
-      }
-      fFunc, err := filter.Init(parts[1], parts[2])
-      if err != nil {
-        return out, fmt.Errorf("Setup failed for filter '%s': %s", parts[0], err)
-      }
-      out = append(out, fFunc)
-    default:
-      return out, fmt.Errorf("Wrong format of '%s'", f)
-    }
-  }
-  return out, nil
+	var out []FilterFunc
+	for _, f := range fs {
+		parts := strings.Split(f, FILTERSEP)
+		switch len(parts) {
+		case 1:
+			filter, present := filters[parts[0]]
+			if !present {
+				return out, fmt.Errorf("Filter '%s' not found", parts[0])
+			}
+			fFunc, err := filter.Init("", "")
+			if err != nil {
+				return out, fmt.Errorf("Setup failed for filter '%s': %s", parts[0], err)
+			}
+			out = append(out, fFunc)
+		case 3:
+			filter, present := filters[parts[0]]
+			if !present {
+				return out, fmt.Errorf("Filter '%s' not found", parts[0])
+			}
+			fFunc, err := filter.Init(parts[1], parts[2])
+			if err != nil {
+				return out, fmt.Errorf("Setup failed for filter '%s': %s", parts[0], err)
+			}
+			out = append(out, fFunc)
+		default:
+			return out, fmt.Errorf("Wrong format of '%s'", f)
+		}
+	}
+	return out, nil
 }
 
 func GetFilters() []Filter {
-  var fs []Filter
-  for _, f := range filters {
-    fs = append(fs, f)
-  }
-  return fs
+	var fs []Filter
+	for _, f := range filters {
+		fs = append(fs, f)
+	}
+	return fs
 }
 
 func ApplyFilters(trs []*Torrent, fs []FilterFunc) []*Torrent {
-  var out []*Torrent
-  for ti := 0; ti < len(trs); ti++ {
-    toPass := len(fs)
-    for fi := 0; fi < len(fs); fi++ {
-      if !fs[fi](trs[ti]) {
-        break
-      }
-      toPass -= 1
-    }
-    if toPass == 0 {
-      out = append(out, trs[ti])
-    }
-  }
-  return out
+	var out []*Torrent
+	for ti := 0; ti < len(trs); ti++ {
+		toPass := len(fs)
+		for fi := 0; fi < len(fs); fi++ {
+			if !fs[fi](trs[ti]) {
+				break
+			}
+			toPass -= 1
+		}
+		if toPass == 0 {
+			out = append(out, trs[ti])
+		}
+	}
+	return out
 }
 
 type Category struct {
